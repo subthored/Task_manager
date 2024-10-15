9658d0facb05f6f33a1058543be86d2dd4540767
Eugene Zhikharev <janiqgames@gmail.com>
2024-10-15 10:33:46 +0700

    init step 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="csrf-param" content="_token"/>
+
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Favicon -->
-    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,14 +16,10 @@
 </head>
 
 <body>
-{{--@include('layouts.header')--}}
-{{--@include('flash::message')--}}
+@include('layouts.navigation')
+@include('flash::message')
 <main>
-    <section class="bg-white dark:bg-gray-900">
-        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto">
-            @yield('content')
-        </div>
-    </section>
+    @yield('content')
 </main>
 </body>
 </html>
