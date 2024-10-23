c42e06fc8388de7ceca27b7f65686562aa6cf2e1
Eugene Zhikharev <janiqgames@gmail.com>
2024-10-23 10:34:12 +0700

    again3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <body>
+        Привет от Хекслета!
         <div class="mr-auto place-self-center lg:col-span-7">
             <x-h1 class="font-extrabold text-white xl:text-6xl">Привет от хекслета!</x-h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 text-xl mt-4">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <link rel="preconnect" href="//fonts.bunny.net">
     <link rel="stylesheet" href="//fonts.bunny.net/css?family=figtree:400,500,600&display=swap">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Nunito">
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['./resources/css/app.css', './resources/js/app.js'])
 </head>
 
 <body>
