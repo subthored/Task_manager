6ac9f6ed004cfdafd1cfd8243328bd6eae7c0835
Eugene Zhikharev <janiqgames@gmail.com>
2024-10-23 10:20:15 +0700

    test fix try

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="csrf-param" content="_token"/>
+    <meta name="csrf-param" content="_token">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="//fonts.bunny.net">
+    <link rel="stylesheet" href="//fonts.bunny.net/css?family=figtree:400,500,600&display=swap">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Nunito">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -18,6 +18,7 @@
 <body>
 @include('layouts.header')
 @include('flash::message')
+<a>Привет от хекслета!</a>
 <main>
     <section class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto">
