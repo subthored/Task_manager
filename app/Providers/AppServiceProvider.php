8905874ae6acde2ffa5adfe23feda6d204bb3789
Eugene Zhikharev <janiqgames@gmail.com>
2024-10-31 16:24:41 +0700

    still try fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('production') || $this->app->environment('dev')) {
-            URL::forceScheme('https');
-        }
+        //
     }
 
     /**
