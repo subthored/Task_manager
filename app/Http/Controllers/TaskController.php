0ce24319584782059f900fa09c1da9aa1f8523fc
Eugene Zhikharev <janiqgames@gmail.com>
2024-11-02 14:42:14 +0700

    next tests

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller;
 use App\Http\Requests\TaskRequest;
 use App\Models\TaskStatus;
@@ -13,6 +14,7 @@ use App\Models\Label;
 
 class TaskController extends Controller
 {
+    use AuthorizesRequests;
     protected Collection $taskStatuses;
     protected Collection $users;
     protected Collection $labels;
@@ -24,11 +26,13 @@ class TaskController extends Controller
         $this->labels = Label::all();
     }
 
-    public function index(Request $request)
+    public function index(Request $request, Task $task)
     {
+        $this->authorize('view', $task);
         $filters = $request->input('filter', []);
 
-        $tasks = Task::query()->filterByStatus($filters['status_id'] ?? null)
+        $tasks = Task::query()
+            ->filterByStatus($filters['status_id'] ?? null)
             ->filterByCreatedBy($filters['created_by_id'] ?? null)
             ->filterByAssignedTo($filters['assigned_to_id'] ?? null)
             ->paginate(15);
