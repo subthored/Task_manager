75916f1f361a6adbe43d2ec21ab2b8d5d33043a9
Eugene Zhikharev <janiqgames@gmail.com>
2024-11-02 13:20:23 +0700

    -\-

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <x-h1 class="text-white">{{ __('Создать задачу') }}</x-h1>
-    {{ html()->modelForm($task, 'POST', route('tasks.store', $task))->open() }}
+    {{ html()->modelForm($task, 'POST', route('tasks.store', $task))->id('taskForm')->open() }}
     @include('tasks.form')
     {{ html()->submit( __('Создать'))->class('btn-primary') }}
     {{ html()->closeModelForm() }}
