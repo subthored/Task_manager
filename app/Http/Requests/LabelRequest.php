5741b9f980941406cf815f00a7713a535a34248e
Eugene Zhikharev <janiqgames@gmail.com>
2024-11-02 12:26:22 +0700

    same x2

--- app/Http/Requests/LabelRequest.php
+++ app/Http/Requests/LabelRequest.php
@@ -44,7 +44,7 @@ class LabelRequest extends FormRequest
         return [
             function (Validator $validator) {
                 $errors = $validator->errors();
-                if ($errors->has('name') && $errors->first('name') === 'The name has already been taken') {
+                if ($errors->has('name') && $errors->first('name') === 'Метка с таким именем уже существует') {
                     $errors->forget('name');
                     $errors->add('name', __('Метка с таким именем уже существует'));
                 }
