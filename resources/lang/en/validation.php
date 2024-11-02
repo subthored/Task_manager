1c97f134ff9a71ab5286df746b145b692c210adb
Eugene Zhikharev <janiqgames@gmail.com>
2024-11-02 13:54:23 +0700

    mistake in lang\validation

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -4,7 +4,7 @@ return [
     'confirmed' => ':attribute и подтверждение не совпадают',
     'exists' => 'Это обязательное поле',
     'min' => ['string' => ':attribute должен иметь длину не менее :min символов'],
-    'required' => ['Это обязательное поле'],
+    'required' => 'Это обязательное поле',
     'attributes' => [
         'password' => 'Пароль'
     ]
